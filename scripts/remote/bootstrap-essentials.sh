eb925ff4b85c254fd5c478e9ebca6610fa04e332
GJ Tiquia <gjtiquia@gmail.com>
2026-09-22 09:49:36 +0800

    wip remote/bootstrap-essentials

---
+++ scripts/remote/bootstrap-essentials.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# e = [e]xit if a command exits with non-zero status
+# u = exit if have [u]nbound variables, prevents typos
+# x = print each command and args before e[x]ecution
+set -eux
+
+# checks if git is authenticated
+# authenticated or note returns exit code 1
+# || true lets it run regardless if authenticated or not
+ssh -T git@github.com || true
+
+# write your automation here
+echo "hello world"
